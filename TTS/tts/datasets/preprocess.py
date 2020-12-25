68bad6c29465c5371fed2f6f9ada86b60212de11
flych3r <xavier_sampaio@alu.ufc.br>
2020-12-25 11:02:30 -0300

    ljspeech encoding

--- TTS/tts/datasets/preprocess.py
+++ TTS/tts/datasets/preprocess.py
@@ -129,7 +129,7 @@ def ljspeech(root_path, meta_file):
     txt_file = os.path.join(root_path, meta_file)
     items = []
     speaker_name = "ljspeech"
-    with open(txt_file, 'r') as ttf:
+    with open(txt_file, 'r', encoding='utf-8') as ttf:
         for line in ttf:
             cols = line.split('|')
             wav_file = os.path.join(root_path, 'wavs', cols[0] + '.wav')
