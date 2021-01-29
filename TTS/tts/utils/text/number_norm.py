1df5657b6fa5263507ebd6ee41a320d93dc34e88
flych3r <xavier_sampaio@alu.ufc.br>
2021-01-29 18:53:32 -0300

    changed expand numbers

--- TTS/tts/utils/text/number_norm.py
+++ TTS/tts/utils/text/number_norm.py
@@ -1,14 +1,10 @@
 """ from https://github.com/keithito/tacotron """
 
-import inflect
 from num2words import num2words
 import re
 
-_inflect = inflect.engine()
 _comma_number_re = re.compile(r'([0-9][0-9\,]+[0-9])')
 _decimal_number_re = re.compile(r'([0-9]+\.[0-9]+)')
-# _pounds_re = re.compile(r'£([0-9\,]*[0-9]+)')
-# _dollars_re = re.compile(r'\$([0-9\.\,]*[0-9]+)')
 _reais_re = re.compile(r'R\$([0-9\.\,]*[0-9]+)')
 _time_re = re.compile(r'([0-9]+\:[0-9]+)')
 _date_re = re.compile(r'\d{1,2}\/\d{1,2}\/\d{2,4}')
@@ -30,25 +26,19 @@ def _expand_decimal_point(m):
     return m.group(1).replace('.', ' ponto ')
 
 
-def _expand_reais(m):
+def _expand_number(m):
+    num = m.group(0)
+    return num2words(num, lang='pt_BR')
+
+
+def _expand_ordinal(m):
+    num = m.group(0)
+    return num2words(num, lang='pt_BR', to='ordinal')
+
+
+def _expand_currency(m):
     match = m.group(1)
-    parts = match.split('.')
-    if len(parts) > 2:
-        return match + ' reais'  # Unexpected format
-    reais = int(parts[0]) if parts[0] else 0
-    centavos = int(parts[1]) if len(parts) > 1 and parts[1] else 0
-    if reais and centavos:
-        dollar_unit = 'real' if reais == 1 else 'reais'
-        cent_unit = 'centavo' if centavos == 1 else 'centavos'
-        return '%s %s e %s %s' % (reais, dollar_unit, centavos, cent_unit)
-    elif reais:
-        dollar_unit = 'real' if reais == 1 else 'reais'
-        return '%s %s' % (reais, dollar_unit)
-    elif centavos:
-        cent_unit = 'centavo' if centavos == 1 else 'centavos'
-        return '%s %s' % (centavos, cent_unit)
-    else:
-        return 'zero reais'
+    return num2words(match, lang='pt_BR', to='currency')
 
 
 def _expand_horas(h):
@@ -71,30 +61,9 @@ def _expand_data(d):
     return '%s de %s de %s' % (dia, mes, ano)
 
 
-def _expand_ordinal(m):
-    return _inflect.number_to_words(m.group(0))
-
-
-def _expand_number(m):
-    num = int(m.group(0))
-    # if 1000 < num < 3000:
-    #     if num == 2000:
-    #         return 'two thousand'
-    #     if 2000 < num < 2010:
-    #         return 'two thousand ' + _inflect.number_to_words(num % 100)
-    #     if num % 100 == 0:
-    #         return _inflect.number_to_words(num // 100) + ' hundred'
-    #     return _inflect.number_to_words(num,
-    #                                     andword='',
-    #                                     zero='oh',
-    #                                     group=2).replace(', ', ' ')
-    # return _inflect.number_to_words(num, andword='')
-    return num2words(num, lang='pt_BR')
-
-
 def normalize_numbers(text):
     text = re.sub(_comma_number_re, _remove_commas, text)
-    text = re.sub(_reais_re, _expand_reais, text)
+    text = re.sub(_reais_re, _expand_currency, text)
     text = re.sub(_time_re, _expand_horas, text)
     text = re.sub(_date_re, _expand_data, text)
     text = re.sub(_decimal_number_re, _expand_decimal_point, text)
